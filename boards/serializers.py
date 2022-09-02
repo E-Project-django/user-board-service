e9a8796b3bc8df303b6cde24649b2fc06f589f58
한송희 <98_han@naver.com>
2022-09-02 16:09:15 +0900

    Feat : 직렬화를 위한 serializer 생성

---
+++ boards/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import Freeboard, Notice, Staffboard
+
+
+class FreeboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Freeboard
+        fields = '__all__'
+
+
+class NoticeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
+        fields = '__all__'
+
+
+class StaffboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staffboard
+        fiedls = '__all__'
