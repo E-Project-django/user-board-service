6ae145a8ebcf25f4595831926e7963d7a041e19e
한송희 <98_han@naver.com>
2022-09-02 23:45:53 +0900

    Feat : 공지사항, 운영자게시판 커스텀 구현 - 공지사항 권한을 위해 AuthorAndStaffAllOnly의 커스텀 권한 구현 - 운영자게시판 권한을 위해 AuthorAndStaffAllOnlyOrReadOnly의 커스텀 권한 구현

--- boards/views.py
+++ boards/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 
 from .models import Freeboard, Notice, Staffboard
 from .serializers import FreeboardSerializer, NoticeSerializer, StaffboardSerializer
-from core.permissions import AuthorAndStaffAllEditOrReadOnly
+from core.permissions import AuthorAndStaffAllEditOrReadOnly,  AuthorAndStaffAllOnly, AuthorAndStaffAllOnlyorReadOnly
 
 
 class FreeboardViewset(viewsets.ModelViewSet):
@@ -15,11 +15,15 @@ class FreeboardViewset(viewsets.ModelViewSet):
 
 class NoticeViewset(viewsets.ModelViewSet):
 
+    permission_classes = [AuthorAndStaffAllOnlyorReadOnly]
+
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
 
 
 class StaffboardViewset(viewsets.ModelViewSet):
+
+    permission_classes = [AuthorAndStaffAllOnly]
 
     queryset = Staffboard.objects.all()
     serializer_class = StaffboardSerializer
--- core/permissions.py
+++ core/permissions.py
@@ -1,13 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+EDIT_METHODS = ("PUT", "PATCH")
 
 class AuthorAndStaffAllEditOrReadOnly(BasePermission):
     """
-     작성자와 관리자(superuser)는 게시글에 대한 전체 액세스 권한을 가지고,
+     작성자(user)와 관리자(superuser)는 게시글에 대한 전체 액세스 권한을 가지고,
      운영자(staff)는 게시판을 삭제할 수 있지만 편집할 수 없도록 Custom한 Permission
     """
-
-    edit_methods = ("PUT", "PATCH")
 
     def has_permission(self, request, view):
         # 사용자가 로그인되어 있을 때만 create 가능
@@ -23,8 +22,45 @@ class AuthorAndStaffAllEditOrReadOnly(BasePermission):
         if obj.author_id == request.user:
             return True
 
-        if request.user.is_staff and request.method not in self.edit_methods:
+        if request.user.is_staff and request.method not in EDIT_METHODS:
             # 운영자는 게시판을 편집할 수 없음
             return True
 
         return False
+
+
+class AuthorAndStaffAllOnly(BasePermission):
+    """
+     운영자(staff)와 관리자(superuser)는 게시글에 대한 전체 액세스 권한을 가지고,
+     작성자(user)는 접근할 수 없도록 Custom한 Permission
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+
+        if obj.author_id == request.user:
+            return True
+
+
+class AuthorAndStaffAllOnlyorReadOnly(BasePermission):
+    """
+     운영자(staff)와 관리자(superuser)는 게시글에 대한 전체 액세스 권한을 가지고,
+     작성자(user)는 읽기만 가능하게 Custom한 Permission
+    """
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or request.user and request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+
+        if obj.author_id == request.user:
+            return True
+
+        if request.user and request.method not in EDIT_METHODS:
+            return True
