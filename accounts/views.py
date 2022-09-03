8bee3732f885ea1596be36a2835b8578229f661d
Blessian <blessian.dev@gmail.com>
2022-09-04 04:37:53 +0900

    hotfix: serializers.py views.py 주석 추가

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,20 @@ from .serializers import RegisterSerializer, UserSerializer, MakeStaffSerializer
 
 
 class RegisterAPI(generics.CreateAPIView):
+    """
+    회원가입 API
+    회원 생성과 토큰 생성
+    """
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
 
 class LoginAPI(generics.GenericAPIView):
+    """
+    로그인 API
+    username, password, token으로 유효성 판단
+    token을 리턴함
+    """
     serializer_class = LoginSerializer
 
     def post(self, request):
@@ -20,18 +29,29 @@ class LoginAPI(generics.GenericAPIView):
 
 
 class AccountAPI(generics.RetrieveUpdateAPIView):
+    """
+    회원 조회, 회원 정보 수정 API
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'username'
 
 
 class MakeStaffAPI(generics.UpdateAPIView):
+    """
+    관리자 임명 API
+    is_staff를 변경
+    """
     queryset = User.objects.all()
     serializer_class = MakeStaffSerializer
     lookup_field = 'username'
 
 
 class SoftDeleteAPI(generics.UpdateAPIView):
+    """
+    sof-delete를 위한 API
+    is_active를 변경
+    """
     queryset = User.objects.all()
     serializer_class = SoftDeleteSerializer
     lookup_field = 'username'
