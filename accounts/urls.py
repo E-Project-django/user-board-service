e52df5c22b3f81737dbe4d94b786d473386d4892
Blessian <blessian.dev@gmail.com>
2022-09-05 22:38:33 +0900

    fix: accounts.psermission.py 수정 - 변수 타입 입력이 잘못 되었음

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,10 @@ from .views import RegisterAPI, LoginAPI, AccountListAPI, AccountDetailAPI, Make
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', RegisterAPI.as_view()),
-    path('login/', LoginAPI.as_view()),
-    path('list/', AccountListAPI.as_view()),
-    path('<str:username>/', AccountDetailAPI.as_view()),
-    path('make_staff/<str:username>/', MakeStaffAPI.as_view()),
-    path('delete/<str:username>/', SoftDeleteAPI.as_view()),
+    path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('list/', AccountListAPI.as_view(), name='list'),
+    path('<str:username>/', AccountDetailAPI.as_view(), name='detail'),
+    path('make_staff/<str:username>/', MakeStaffAPI.as_view(), name='make_staff'),
+    path('delete/<str:username>/', SoftDeleteAPI.as_view(), name='delete'),
 ]
