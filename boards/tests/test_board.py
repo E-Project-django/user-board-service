12ef1dd47fc7bc4d45488c9f0856479ff6b33033
한송희 <98_han@naver.com>
2022-09-05 15:04:02 +0900

    Test : 공지사항 CRUD API Testcase 작성

--- boards/tests/test_board.py
+++ boards/tests/test_board.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from boards.models import Freeboard
+from boards.models import Freeboard, Notice
 User = get_user_model()
 
 
@@ -17,7 +17,7 @@ class FreeboardViewSetTest(APITestCase):
         )
         self.client.login(username='user', password='0000')
         self.freeboard = Freeboard.objects.create(
-            title='자유게시판 제목 테스트 첫번째입니다.', content='자유게시판 본문 테스트 두번째입니다.', author_id=self.user
+            title='자유게시판 제목 테스트 첫번째입니다.', content='자유게시판 본문 테스트 첫번째입니다.', author_id=self.user
         )
 
     def test_user_create(self):
@@ -56,4 +56,55 @@ class FreeboardViewSetTest(APITestCase):
 
     def test_user_destory(self):
         response = self.client.delete(reverse('freeboard-detail', kwargs={'pk': 1}))
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class NoticeViewSetTest(APITestCase):
+    """ 공지사항 Viewset action(list,create,retrieve,update,partial_update,destory)이 정상 작동하는지 확인하기 위한 테스트 """
+
+    def setUp(self):
+        self.staff = User.objects.create_user(
+            username='staff', password='0000', age=25, gender='F', phone_number='010-1234-5678', is_staff= True
+        )
+        self.client.login(username='staff', password='0000')
+        self.notice = Notice.objects.create(
+            title='공지사항 제목 테스트 첫번째입니다.', content='공지사항 본문 테스트 첫번째입니다.', author_id=self.staff
+        )
+
+    def test_user_create(self):
+        data = {
+            'title': '공지사항 제목 테스트 두번째입니다.',
+            'content': '공지사항 본문 테스트 두번째입니다.',
+            'author_id': 1
+        }
+
+        response = self.client.post(reverse('notice-list'), data)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_get_list(self):
+        response = self.client.get(reverse('notice-list'))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_user_get_retrieve(self):
+        response = self.client.get(reverse('notice-detail', kwargs={'pk': 1}))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_user_update(self):
+        update_data = {
+            'title': '공지사항 제목 수정중입니다.',
+            'content': '공지사항 본문 수정중입니다.',
+            'author_id': 1
+        }
+        response = self.client.put(reverse('notice-detail', kwargs={'pk': 1}), update_data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_user_partial_update(self):
+        partial_update_data = {
+            'content': '공지사항 본문 내용만 수정중입니다.',
+        }
+        response = self.client.patch(reverse('notice-detail', kwargs={'pk': 1}), partial_update_data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_user_destory(self):
+        response = self.client.delete(reverse('notice-detail', kwargs={'pk': 1}))
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
